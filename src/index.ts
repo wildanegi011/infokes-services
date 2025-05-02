2bdf984d3b8b736f7c1de0c557094116e3b2ab58
wildan.egi <w.egi@bdg.ebdesk.com>
2025-05-02 12:20:35 +0700

    feat: inject usecase to handler

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { Elysia } from "elysia";
-import { folderItemRoutes } from "./app/modules/folder_item/handler";
+import { createFolderItemRoutes } from "./app/modules/folder_item/handler";
 import { APP_URL } from "./constants";
 import cors from "@elysiajs/cors";
+import { FolderItemUsecase } from "./app/modules/folder_item/usecase";
+import { FolderItemRepository } from "./app/modules/folder_item/repository";
 
 new Elysia()
   .use(cors())
-  .group("/api/v1", (app) => app.use(folderItemRoutes))
+  .group("/api/v1", (app) =>
+    app.use(
+      createFolderItemRoutes(new FolderItemUsecase(new FolderItemRepository()))
+    )
+  )
   .listen(3000, () => {
     console.log(`Server running on ${APP_URL}`);
   });
